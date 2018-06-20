8f221ee52e48d375b96ab34eafc0342760c5ecb7
Iwan Teplyakov <iteplyakov@icloud.com>
2018-06-20 15:36:48 +0300

    Initial Commit

---
+++ Page/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Page
+//
+//  Created by Iwan Teplyakov on 6/20/18.
+//  Copyright © 2018 Iwan Teplyakov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
