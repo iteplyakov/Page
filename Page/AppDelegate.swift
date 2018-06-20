2e8d4f2eaabf3b946398a0d4f65c20e4bd0333e7
Iwan Teplyakov <iteplyakov@icloud.com>
2018-06-20 18:10:39 +0300

    Go

--- Page/AppDelegate.swift
+++ Page/AppDelegate.swift
@@ -8,39 +8,55 @@
 
 import UIKit
 
-@UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
+@UIApplicationMain class ApplicationCoordinator: UIResponder {
 
     var window: UIWindow?
 
-
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
-        return true
-    }
-
-    func applicationWillResignActive(_ application: UIApplication) {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
-    }
-
-    func applicationDidEnterBackground(_ application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-    }
-
-    func applicationWillEnterForeground(_ application: UIApplication) {
-        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
-    }
-
-    func applicationDidBecomeActive(_ application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-    }
-
-    func applicationWillTerminate(_ application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-    }
-
-
 }
 
+
+extension ApplicationCoordinator: UIApplicationDelegate {
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        guard case let pageViewController as UIPageViewController = window?.rootViewController else {
+            preconditionFailure("Initial view controller not UIPageViewController")
+        }
+        pageViewController.dataSource = self
+        pageViewController.delegate = self
+        pageViewController.storyboard.map {
+            pageViewController.setViewControllers(
+                [
+                    $0.instantiateViewController(withIdentifier: "VC")
+                ],
+                direction: .forward,
+                animated: false
+            )
+        }
+        return true
+    }
+    
+}
+
+extension ApplicationCoordinator: UIPageViewControllerDelegate {
+    
+}
+
+extension ApplicationCoordinator: UIPageViewControllerDataSource {
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+        return nil
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+        return pageViewController.storyboard?.instantiateViewController(withIdentifier: "VC")
+    }
+    
+    func presentationCount(for pageViewController: UIPageViewController) -> Int {
+        return 10
+    }
+    
+    func presentationIndex(for pageViewController: UIPageViewController) -> Int {
+        return Int.random(in: 0...10)
+    }
+}
